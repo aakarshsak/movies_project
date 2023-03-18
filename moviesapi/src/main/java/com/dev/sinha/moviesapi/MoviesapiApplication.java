08d6dca0f25589157ea42373edbb9c6e2c3ed158
Aakarsh Sinha <aakarshsak@gmail.com>
2023-03-18 20:19:18 +0530

    Setting up spring boot and adding get all movies endpoint

---
+++ moviesapi/src/main/java/com/dev/sinha/moviesapi/MoviesapiApplication.java
@@ -0,0 +1,17 @@
+package com.dev.sinha.moviesapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesapiApplication.class, args);
+	}
+
+}
