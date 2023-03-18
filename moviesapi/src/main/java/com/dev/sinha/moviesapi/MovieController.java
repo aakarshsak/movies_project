1c9dc144864748f3cec6ed93c4dd3e7d02155ca6
Aakarsh Sinha <aakarshsak@gmail.com>
2023-03-18 21:32:20 +0530

    adding other endpooints and completing the basic application backend

--- moviesapi/src/main/java/com/dev/sinha/moviesapi/MovieController.java
+++ moviesapi/src/main/java/com/dev/sinha/moviesapi/MovieController.java
@@ -1,13 +1,14 @@
 package com.dev.sinha.moviesapi;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,6 +20,16 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable ObjectId id) {
+        return new ResponseEntity(movieService.singleMovie(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/imdb/{id}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String id) {
+        return new ResponseEntity(movieService.singleMovieWithImdbId(id), HttpStatus.OK);
     }
 
 }
--- moviesapi/src/main/java/com/dev/sinha/moviesapi/MovieService.java
+++ moviesapi/src/main/java/com/dev/sinha/moviesapi/MovieService.java
@@ -1,9 +1,14 @@
 package com.dev.sinha.moviesapi;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Service
 public class MovieService {
@@ -13,5 +18,14 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> singleMovieWithImdbId(String id) {
+        System.out.println(id);
+        return movieRepository.findMovieByImdbId(id);
     }
 }
